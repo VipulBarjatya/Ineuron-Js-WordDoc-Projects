2e188d589bb2af39c1332fc8f15bdd3ecbed8931
Vipul <vipulbarjatya@gmail.com>
2022-11-23 22:57:17 +0530

    text generator api

---
+++ Typing game/script.js
@@ -0,0 +1,23 @@
+const givenTextEl = document.getElementById("givenText");
+const textInputEl = document.getElementById("textInput");
+
+const randomQuote = "https://api.quotable.io/random";
+
+function getRandomQuote() {
+  return fetch(randomQuote)
+    .then((resp) => resp.json())
+    .then((data) => data.content);
+}
+
+async function renderNewQuote() {
+  const quote = await getRandomQuote();
+  givenTextEl.innerText = "";
+  quote.split("").forEach((character) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    givenTextEl.appendChild(characterSpan);
+  });
+  textInputEl.value = null;
+}
+
+renderNewQuote();
