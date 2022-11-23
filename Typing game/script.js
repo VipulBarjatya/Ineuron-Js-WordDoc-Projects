f1fb5b2a32c10fcd2f7aba8ba2b32c53e7b92884
Vipul <vipulbarjatya@gmail.com>
2022-11-23 23:20:53 +0530

    adding classes dynamically

--- Typing game/script.js
+++ Typing game/script.js
@@ -2,12 +2,25 @@ const givenTextEl = document.getElementById("givenText");
 const textInputEl = document.getElementById("textInput");
 const randomQuote = "https://api.quotable.io/random";
 
-textInputEl.addEventListener("input", () => {});
+textInputEl.addEventListener("input", () => {
+  const arrayText = givenTextEl.querySelectorAll("span");
+  const arrayValue = textInputEl.value.split("");
+  arrayText.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character === characterSpan.innerText) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.add("incorrect");
+    }
+  });
+});
 
-function getRandomQuote() {
-  return fetch(randomQuote)
-    .then((resp) => resp.json())
-    .then((data) => data.content);
+async function getRandomQuote() {
+  const resp = await fetch(randomQuote);
+  const data = await resp.json();
+  return data.content;
 }
 
 async function renderNewQuote() {
