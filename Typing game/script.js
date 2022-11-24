3453f8d78d2a28e36aab817f61b2ad95a40151be
Vipul <vipulbarjatya@gmail.com>
2022-11-24 08:42:09 +0530

    render new on correct

--- Typing game/script.js
+++ Typing game/script.js
@@ -5,16 +5,24 @@ const randomQuote = "https://api.quotable.io/random";
 textInputEl.addEventListener("input", () => {
   const arrayText = givenTextEl.querySelectorAll("span");
   const arrayValue = textInputEl.value.split("");
+  let correct = true;
   arrayText.forEach((characterSpan, index) => {
     const character = arrayValue[index];
-    if (character === characterSpan.innerText) {
+    if (character == null) {
+      // characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
       characterSpan.classList.add("correct");
       characterSpan.classList.remove("incorrect");
     } else {
       characterSpan.classList.remove("correct");
       characterSpan.classList.add("incorrect");
+      correct = false;
     }
   });
+
+  if (correct) renderNewQuote();
 });
 
 async function getRandomQuote() {
