6bd7880d10b42b234c52b090a51c8159c6095e2e
Vipul <vipulbarjatya@gmail.com>
2022-11-24 12:01:22 +0530

    word count functionality done

--- Word-Count-App/script.js
+++ Word-Count-App/script.js
@@ -1,1 +1,14 @@
 const textInputEl = document.getElementById("textInput");
+const countBtn = document.querySelector(".count");
+const clrBtn = document.querySelector(".clear");
+const counterEl = document.querySelector(".counter");
+
+countBtn.addEventListener("click", () => {
+  const wordArr = textInputEl.value.split(" ");
+  counterEl.textContent = wordArr.length;
+});
+
+clrBtn.addEventListener("click", () => {
+  textInputEl.value = "";
+  counterEl.textContent = 0;
+});
